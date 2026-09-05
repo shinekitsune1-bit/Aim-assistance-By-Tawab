c7afb2e4d5aa0ce796d05c69b67ed229d1591239
shinekitsune1-bit <shinekitsune1@gmail.com>
2026-09-05 14:48:27 +0500

    Add AimAssistConfig class with default settings

---
+++ src/main/java/com/tawab/aimassist/AimAssistConfig.java
@@ -0,0 +1,13 @@
+package com.tawab.aimassist;
+
+public class AimAssistConfig {
+
+    public static boolean enabled = false;
+
+    public static double strength = 50.0;
+    public static double distance = 6.0;
+    public static double smoothness = 50.0;
+
+    private AimAssistConfig() {
+    }
+}
