d30a0210a8b32ced5fa6d5a152296b36ef58b59a
shinekitsune1-bit <shinekitsune1@gmail.com>
2026-09-05 14:50:57 +0500

    Implement sliders for aim assist configuration
    Added sliders for strength, distance, and smoothness adjustments in the Aim Assist screen.

--- src/main/java/com/tawab/aimassist/AimAssistScreen.java
+++ src/main/java/com/tawab/aimassist/AimAssistScreen.java
@@ -3,9 +3,14 @@ package com.tawab.aimassist;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.SliderWidget;
 import net.minecraft.text.Text;
 
 public class AimAssistScreen extends Screen {
+
+    private StrengthSlider strengthSlider;
+    private DistanceSlider distanceSlider;
+    private SmoothnessSlider smoothnessSlider;
 
     public AimAssistScreen() {
         super(Text.literal("Aim Assist By Tawab"));
@@ -19,31 +24,54 @@ public class AimAssistScreen extends Screen {
         this.addDrawableChild(
                 ButtonWidget.builder(
                         Text.literal(
-                                "Aim Assist: " +
-                                        (AimAssistClient.isAimAssistEnabled()
-                                                ? "ON"
-                                                : "OFF")
+                                AimAssistConfig.enabled
+                                        ? "Aim Assist: ON"
+                                        : "Aim Assist: OFF"
                         ),
                         button -> {
-
-                            AimAssistClient.toggle();
+                            AimAssistConfig.enabled =
+                                    !AimAssistConfig.enabled;
 
                             button.setMessage(
                                     Text.literal(
-                                            "Aim Assist: " +
-                                                    (AimAssistClient.isAimAssistEnabled()
-                                                            ? "ON"
-                                                            : "OFF")
+                                            AimAssistConfig.enabled
+                                                    ? "Aim Assist: ON"
+                                                    : "Aim Assist: OFF"
                                     )
                             );
                         }
                 ).dimensions(
                         centerX - 100,
-                        80,
+                        60,
                         200,
                         20
                 ).build()
         );
+
+        strengthSlider = new StrengthSlider(
+                centerX - 100,
+                95,
+                200,
+                20
+        );
+
+        distanceSlider = new DistanceSlider(
+                centerX - 100,
+                130,
+                200,
+                20
+        );
+
+        smoothnessSlider = new SmoothnessSlider(
+                centerX - 100,
+                165,
+                200,
+                20
+        );
+
+        this.addDrawableChild(strengthSlider);
+        this.addDrawableChild(distanceSlider);
+        this.addDrawableChild(smoothnessSlider);
 
         this.addDrawableChild(
                 ButtonWidget.builder(
@@ -51,7 +79,7 @@ public class AimAssistScreen extends Screen {
                         button -> close()
                 ).dimensions(
                         centerX - 100,
-                        120,
+                        205,
                         200,
                         20
                 ).build()
@@ -77,7 +105,7 @@ public class AimAssistScreen extends Screen {
                 this.textRenderer,
                 this.title,
                 this.width / 2,
-                40,
+                30,
                 0xFFFFFF
         );
 
@@ -88,42 +116,126 @@ public class AimAssistScreen extends Screen {
                 delta
         );
     }
-            }                )
-        );
 
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+    private static class StrengthSlider extends SliderWidget {
 
-            while (toggleKey.wasPressed()) {
-                toggle();
+        StrengthSlider(
+                int x,
+                int y,
+                int width,
+                int height
+        ) {
+            super(
+                    x,
+                    y,
+                    width,
+                    height,
+                    Text.literal(""),
+                    AimAssistConfig.strength / 100.0
+            );
 
-                if (client.player != null) {
-                    client.player.sendMessage(
-                            net.minecraft.text.Text.literal(
-                                    "Aim Assist: " +
-                                            (aimAssistEnabled ? "ON" : "OFF")
-                            ),
-                            true
-                    );
-                }
+            updateMessage();
+        }
+
+        @Override
+        protected void updateMessage() {
+            setMessage(
+                    Text.literal(
+                            "Strength: " +
+                                    Math.round(value * 100) +
+                                    "%"
+                    )
+            );
+        }
+
+        @Override
+        protected void applyValue() {
+            AimAssistConfig.strength =
+                    value * 100.0;
+        }
+    }
+
+    private static class DistanceSlider extends SliderWidget {
+
+        DistanceSlider(
+                int x,
+                int y,
+                int width,
+                int height
+        ) {
+            super(
+                    x,
+                    y,
+                    width,
+                    height,
+                    Text.literal(""),
+                    (AimAssistConfig.distance - 1.0) / 19.0
+            );
+
+            updateMessage();
+        }
+
+        @Override
+        protected void updateMessage() {
+
+            double distance =
+                    1.0 + value * 19.0;
+
+            setMessage(
+                    Text.literal(
+                            String.format(
+                                    "Distance: %.1f blocks",
+                                    distance
+                            )
+                    )
+            );
+        }
+
+        @Override
+        protected void applyValue() {
+
+            AimAssistConfig.distance =
+                    1.0 + value * 19.0;
+        }
+    }
+
+    private static class SmoothnessSlider extends SliderWidget {
+
+        SmoothnessSlider(
+                int x,
+                int y,
+                int width,
+                int height
+        ) {
+            super(
+                    x,
+                    y,
+                    width,
+                    height,
+                    Text.literal(""),
+                    AimAssistConfig.smoothness / 100.0
+            );
+
+            updateMessage();
+        }
+
+        @Override
+        protected void updateMessage() {
+
+            setMessage(
+                    Text.literal(
+                            "Smoothness: " +
+                                    Math.round(value * 100) +
+                                    "%"
+                    )
+            );
+        }
+
+        @Override
+        protected void applyValue() {
+
+            AimAssistConfig.smoothness =
+                    value * 100.0;
+        }
+    }
             }
-
-            while (settingsKey.wasPressed()) {
-                if (client.currentScreen == null) {
-                    client.setScreen(new AimAssistScreen());
-                }
-            }
-        });
-
-        System.out.println(
-                "[Aim Assist By Tawab] Loaded successfully!"
-        );
-    }
-
-    public static void toggle() {
-        aimAssistEnabled = !aimAssistEnabled;
-    }
-
-    public static boolean isAimAssistEnabled() {
-        return aimAssistEnabled;
-    }
-}
